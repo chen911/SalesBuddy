ead36e36192ea11fe845c316a99c47884f7076b5
chenith <chenith@oneit.com.au>
2017-06-07 21:06:32 +0530

    item list search

--- src/pages/item-list/item-list.ts
+++ src/pages/item-list/item-list.ts
@@ -11,6 +11,7 @@ import { ItemProvider } from '../../providers/item/item';
 })
 export class ItemListPage {
   public itemList: Array<any>;
+  public loadedItemList:Array<any>;
 
   constructor(public navCtrl: NavController, public itemProvider: ItemProvider) {}
 
@@ -27,6 +28,35 @@ export class ItemListPage {
         });
         return false;
       });
+
+      this.loadedItemList = this.itemList;
     });
+  }
+
+    initializeItems(){
+    this.itemList = this.loadedItemList;
+  }
+
+  searchItem(searchbar) {
+    // Reset items back to all of the items
+    this.initializeItems();
+    // set q to the value of the searchbar
+    var q = searchbar.srcElement.value;
+    // if the value is an empty string don't filter the items
+    if (!q) {
+      return;
+    }
+
+    this.itemList = this.itemList.filter((v) => {
+      if(v.name && q) {
+        if (v.name.toLowerCase().indexOf(q.toLowerCase()) > -1 
+              || (v.code && v.code.toLowerCase().indexOf(q.toLowerCase()) > -1)) {
+          return true;
+        }
+        return false;
+      }
+    });
+
+    console.log(q, this.itemList.length);
   }
 }
